240c4304a75f22f223f6684d05afeb15f4738da4
Johandev <162395710+HanjoBulKing7@users.noreply.github.com>
2026-07-17 11:10:00 -0600

    chores: Added project initialized

---
+++ src/main/java/com/example/ArpegioBackend/ArpegioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArpegioBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArpegioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArpegioBackendApplication.class, args);
+	}
+
+}
